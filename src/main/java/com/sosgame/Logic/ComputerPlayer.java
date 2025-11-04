41589c1120a796825242cd9c8c2d281d90b4d6a8
Jai Patel <jaipatel4717@gmail.com>
2025-11-04 12:19:00 -0600

    Add ComputerPlayer class to implement computer-controlled player

---
+++ src/main/java/com/sosgame/Logic/ComputerPlayer.java
@@ -0,0 +1,8 @@
+package com.sosgame.Logic;
+
+public class ComputerPlayer extends Player{
+    // Constructor to create a new computer player
+    public ComputerPlayer(String color) {
+        super(color, "Computer");
+    }
+}
