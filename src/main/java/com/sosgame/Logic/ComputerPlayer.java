6309d504c68d3ccdd8475c1e994433b1100d8ea8
JaiPatel31 <149627486+JaiPatel31@users.noreply.github.com>
2025-11-04 18:13:21 -0600

    Add move selection logic for ComputerPlayer using random placement

--- src/main/java/com/sosgame/Logic/ComputerPlayer.java
+++ src/main/java/com/sosgame/Logic/ComputerPlayer.java
@@ -1,8 +1,23 @@
 package com.sosgame.Logic;
 
+import java.util.Random;
+
 public class ComputerPlayer extends Player{
     // Constructor to create a new computer player
     public ComputerPlayer(String color) {
         super(color, "Computer");
+    }
+
+
+    public int[] chooseMove(GameBoard board) {
+        Random rand = new Random();
+        int row, col;
+
+        do {
+            row = rand.nextInt(board.getSize());
+            col = rand.nextInt(board.getSize());
+        } while (!board.isCellEmpty(row, col));
+
+        return new int[]{row, col};
     }
 }
